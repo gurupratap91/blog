26c2ddaf942cde0e1a5f7c7cff39686dee98803d
Gurupratap <gurupratap@codeignition.co>
2015-06-02 16:24:50 +0530

    Gurupratap: Article model added, create action added with 'improper' tests

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,15 @@ class ArticlesController < ActionController::Base
   def new
     render template: "articles/new"
   end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -3,11 +3,35 @@ require 'rails_helper'
 RSpec.describe ArticlesController, type: :controller do
 
   describe 'new' do
-   subject {get :new}
+    subject {get :new}
     it "should render the new article page" do
       #expect(get :new).to render_template(:new)
       expect(subject).to have_http_status(:success)
       expect(subject).to render_template(:new)
+    end
+  end
+
+  describe 'create' do
+    context "when new article is posted" do
+      subject {post :new}
+      it 'should perform the create action of articles' do
+        expect(subject).to have_http_status(:success)
+        #expect(subject).to render_template(:new)
+      end
+    end
+
+    context "when initialized" do
+      subject(:article) {Article.new}
+      it 'is a new article' do
+        expect(article).to be_a_new(Article)
+      end
+    end
+
+    context " redirects to show" do
+      subject { post :create, :article => { :title => "Foo", :text => "Bar" } }
+      it "redirects to article_url(@article)" do
+        expect(subject).to redirect_to(article_url(assigns(:article)))
+      end
     end
   end
 end
